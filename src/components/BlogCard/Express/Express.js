ec31750a7d2adcd1c5e7b6858bdecd66bcc723dd
Edik01 <128157224+Edik01@users.noreply.github.com>
2023-09-30 11:45:15 -0700

    update

--- src/components/BlogCard/Express/Express.js
+++ src/components/BlogCard/Express/Express.js
@@ -1,17 +1,17 @@
-export const express = require('express');
-const app = express();
+// export const express = require('express');
+// const app = express();
 
-// Middleware для получения IP-адреса клиента
-app.use((req, res, next) => {
-  const clientIp = req.ip; // Получаем IP-адрес клиента
-  console.log(`IP-адрес клиента: ${clientIp}`);
-  next(); // Переходим к следующему middleware или обработчику маршрута
-});
+// // Middleware для получения IP-адреса клиента
+// app.use((req, res, next) => {
+//   const clientIp = req.ip; // Получаем IP-адрес клиента
+//   console.log(`IP-адрес клиента: ${clientIp}`);
+//   next(); // Переходим к следующему middleware или обработчику маршрута
+// });
 
-app.get('/', (req, res) => {
-  res.send('Привет, мир!');
-});
+// app.get('/', (req, res) => {
+//   res.send('Привет, мир!');
+// });
 
-app.listen(3000, () => {
-  console.log('Сервер запущен на порту 3000');
-});
+// app.listen(3000, () => {
+//   console.log('Сервер запущен на порту 3000');
+// });
